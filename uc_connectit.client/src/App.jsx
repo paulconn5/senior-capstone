1f5fa98a8a18128f94cfdd6bec4ca2a7b3693d2b
Paul Connolly <connolpe@mail.uc.edu>
2025-11-19 16:50:59 -0500

    Updated organization

--- uc_connectit.client/src/App.jsx
+++ uc_connectit.client/src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google'
-import Login from './Login'
-import NotFound from './NotFound'
-import './App.css'
+import Login from './pages/Login'
+import NotFound from './pages/NotFound'
+import './styles/App.css'
+
 
 // You'll need to replace this with your actual Google Client ID
 // Get it from: https://console.cloud.google.com/apis/credentials
